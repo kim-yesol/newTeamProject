5d6e5b526bfbd03b7c767a82a0d488b2eb959582
yesol <catz96@naver.com>
2022-05-11 20:07:42 +0900

    1111

--- src/main/java/com/teamproject/myweb/Controller/userController.java
+++ src/main/java/com/teamproject/myweb/Controller/userController.java
@@ -1,6 +1,5 @@
 package com.teamproject.myweb.Controller;
 
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,37 +11,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
-import com.teamproject.myweb.command.ExamineVO;
+import com.teamproject.myweb.Service.boardService;
 import com.teamproject.myweb.command.UserCheckVO;
 import com.teamproject.myweb.command.UserVO;
 import com.teamproject.myweb.user.UserService;
-
-
-import com.teamproject.myweb.command.UserCheckVO;
-import com.teamproject.myweb.command.UserVO;
-import com.teamproject.myweb.review.boardService;
-
 
 @Controller
 @RequestMapping("/user")
 public class userController {
 	
 	@Autowired
-	boardService boardservice;
-	
-	@Autowired
 	UserService userService;
 	
 	@GetMapping("/myPage")
-	public String myPage() {
+	public String myPage(@RequestParam("user_no") int user_no,
+						 Model model) {
+		
+		UserVO userVO = userService.myPage(user_no);
+		model.addAttribute("userVO", userVO);
 		
 		return "user/myPage";
 	}
 	
 	@GetMapping("/myModify")
 	public String myModify() {
+		
+		
 		return "user/myModify";
+	}
+	
+	@PostMapping("/modify")
+	public String modify(UserVO vo) {
+		System.out.println(vo.toString());
+		userService.modify(vo);
+		
+		return "redirect:/main";
 	}
 	
 	@GetMapping("/userLogin")
@@ -54,8 +57,7 @@ public class userController {
 	public String userCheck(HttpSession session, UserCheckVO vo,Model model) {
 		
 		System.out.println(vo.toString());
-		UserVO userVO = boardservice.userCheckes(vo);
-		
+		UserVO userVO = userService.userCheckes(vo);
 		if(userVO == null) {
 			model.addAttribute(vo);
 			return "user/userLogin";
@@ -71,24 +73,36 @@ public class userController {
 		return "user/userJoin";
 	}
 	
+	@PostMapping("/joinform")
+	public String joinform(UserVO vo) {
+		userService.join(vo);
+		return "redirect:/user/userLogin";
+	}
+	
 	@GetMapping("/userFind")
 	public String userFind() {
 		return "user/userFind";
 	}
 	
-	@GetMapping("/examine")
-	public String examine() {
-		return "user/examine";
+	@PostMapping("/userDelete")
+	public String userDelete(@RequestParam("user_no") int user_no, RedirectAttributes RA) {
+		
+		int result = userService.userDelete(user_no);
+		
+		if(result == 1) {
+			RA.addFlashAttribute("msg", "탈퇴 되었습니다");
+		} else {
+			RA.addFlashAttribute("msg", "탈퇴에 실패 했습니다");
+		}
+		
+		return "redirect:/main";
 	}
 	
-	@GetMapping("/examineForm")
-	public String examineForm(ExamineVO vo, RedirectAttributes RA) {
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
 		
-		int result = userService.examine(vo);
-		if(result == 1) {
-			RA.addFlashAttribute("msg", "제출이 완료되었습니다 감사합니다");
-		}
-		System.out.println(vo.toString());
+		session.invalidate();
+		
 		return "redirect:/main";
 	}
 	
